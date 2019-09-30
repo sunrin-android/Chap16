f2e2ba01b7cb7b63f4e24e2c90fc0ef857321ac1
heptacode <admin@hyunwoo.kim>
2019-09-30 14:58:30 +0900

    Modify actionBar

--- app/src/main/java/com/example/chap16/MainActivity.java
+++ app/src/main/java/com/example/chap16/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.chap16;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    Boolean isHidden = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +22,29 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.button);
 
         final ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("액션바 실습");
+        actionBar.setSubtitle("서브 타이틀");
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.drawable.icon);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                actionBar.hide();
+                if (isHidden) {
+                    actionBar.show();
+                } else {
+                    actionBar.hide();
+                }
+                isHidden = !isHidden;
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == android.R.id.home)
+            Toast.makeText(this, "HomeAsUpEnabled", Toast.LENGTH_SHORT).show();
+        return true;
     }
 }
